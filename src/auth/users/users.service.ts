22ac8f76c53c7e3290f85b91eb1476b4bdcf9386
Bbvmex <hugovelasmar@gmail.com>
2024-06-21 02:33:14 -0300

    Fim do primeiro dia

---
+++ src/auth/users/users.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserRoles } from './user-roles';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+  constructor(private prismaService: PrismaService) {}
+
+  createPartnerUser(data: CreateUserDto) {
+    return this.prismaService.user.create({
+      data: {
+        ...data,
+        password: this.generateHash(data.password),
+        roles: [UserRoles.PARTNER],
+      },
+    });
+  }
+
+  createCommonUser(data: CreateUserDto) {
+    return this.prismaService.user.create({
+      data: {
+        ...data,
+        password: this.generateHash(data.password),
+        roles: [UserRoles.USER],
+      },
+    });
+  }
+
+  generateHash(password: string) {
+    return bcrypt.hashSync(password, 18);
+  }
+}
